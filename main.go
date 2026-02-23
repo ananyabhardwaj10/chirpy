61771790134f3755c53125326dad8e4ba94fcbdb
ananyabhardwaj10 <ananyabhardwaj0510@gmail.com>
2026-02-23 10:53:52 +0000

    added middleware to count file server hits

--- main.go
+++ main.go
@@ -2,7 +2,30 @@ package main
 
 import(
 	"net/http"
+	"sync/atomic"
+	"fmt"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request){
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, req)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, req *http.Request) {
+	hits := cfg.fileserverHits.Load()
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Write([]byte(fmt.Sprintf("Hits: %d", hits)))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits.Store(0)
+}
 
 func main() {
 	mux := http.NewServeMux()
@@ -12,9 +35,21 @@ func main() {
 		Handler: mux,
 	}
 
+	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
 	handler := http.FileServer(http.Dir("."))
 
-	mux.Handle("/", handler)
+	apiCfg := apiConfig {
+		fileserverHits: atomic.Int32{},
+	}
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", handler)))
+	mux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /reset", apiCfg.handlerReset)
 
 	server.ListenAndServe()
 }
